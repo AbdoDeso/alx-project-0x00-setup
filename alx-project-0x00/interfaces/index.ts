20df64b908d48a435bee60e51cd275b4efa092d3
Abdullah Eldesoky <microstar747@gmail.com>
2025-06-02 12:59:15 +0300

    commit

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -1,10 +1,10 @@
 export interface PillProps {
   title: string
 }
-export type  TitleVariants =  'first' | 'second' | 'last';
-export type  StyleVariants =  'rounded-sm'|'rounded-md'|'rounded-lg';  
+export type  TitleVariants = string| 'first' | 'second' | 'last';
+export type  StyleVariants = string|  'rounded-sm'|'rounded-md'|'rounded-lg';  
 
 export interface ButtonProps {
-    title: TitleVariants,
-    styles: StyleVariants,
+    title:string| TitleVariants,
+    styles?:string| StyleVariants,
 }
