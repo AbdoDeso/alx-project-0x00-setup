5065110f38e38bd43eef9f96afe3449499174ca1
Abdullah Eldesoky <microstar747@gmail.com>
2025-06-02 11:46:23 +0300

    commit

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,29 +1,25 @@
 import { ButtonProps } from "@/interfaces";
-import { title } from "process";
- 
+
 const buttonTitle = {
-    first: "First Value" ,
-    seconed: "Second Value",
-    last: "The Last Value"
+  first: "First Value",
+  second: "Second Value", 
+  last: "The Last Value"
+};
 
-}
 const Style = {
-  small:  "rounded-sm",
-  mid:  "rounded-mid",
-  large:  "rounded-lg",
-}
+  small: "rounded-sm",
+  mid: "rounded-md", 
+  large: "rounded-lg"
+};
 
-const Button: React.FC<ButtonProps> = ({TitleVariants ,StyleVariants}) => {
+const Button: React.FC<ButtonProps> = ({ TitleVariants, StyleVariants }) => {
+  return (
+    <div className="text-2xl p-10 text-black bg-yellow-100 pt-20">
+      <button type="button" className={Style[StyleVariants]}>
+        {buttonTitle[TitleVariants]}
+      </button>
+    </div>
+  );
+};
 
-    return (
-        <div className="text-2xl p-10 text-black  bg-yellow-100 pt-20">
-       
-            <button  type="button" className={`${Style[StyleVariants]}`}>
-               <a href="#"> {buttonTitle[TitleVariants]}</a>
-            </button>
-            
-        </div>
-    )
-
-}
 export default Button;
--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -6,6 +6,6 @@ export interface ButtonProps {
     title: string ,
     styles: string ,
     TitleVariants:  'first' | 'second' | 'last',
-    StyleVariants?: 'small'|'mid'|'large',  
+    StyleVariants: 'small'|'mid'|'large',  
 
 }
